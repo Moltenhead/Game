7603dae0721427dabb5923bb4581ad296eb36294
Moltenhead <moltenhead@mintmydrive.home>
2020-09-20 13:16:50 +0200

    feat: add VariableDisplayer first scene & script

---
+++ Assets/Scripts/Objects/Obj_VariableDisplayer.gd
@@ -0,0 +1,36 @@
+extends Label
+
+onready var IDisplay = $IVariableDisplay
+
+export(int, "Inputs") var source_id = 0
+export(String) var variable_name = ""
+
+var variable_value
+var true_string = ""
+var source_target
+
+func _set_source():
+	match source_id:
+		0:
+			source_target = InputMap
+
+func update_value():
+	match source_id:
+		0:
+			variable_value = source_target.get_action_list(variable_name)[-1]
+
+func _update_data():
+	_set_source()
+	update_value()
+
+func update_text():
+	true_string = str(variable_value)
+	print(true_string)
+	set_text(true_string)
+
+func _on_ready():
+	_update_data()
+	update_text()
+
+func _ready():
+	_on_ready()
